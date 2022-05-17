eb75e69930cab4d2844425c5c1bd96146ddbcc91
mattnorris1990 <mattnorris1990@gmail.com>
2022-05-17 10:08:28 +0100

    added a few more tests

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -9,12 +9,39 @@ describe("Calculator", () => {
     cy.get('.display').should('contain', '2')
   })
 
-  it('do the arithmetical buttons updated the display with the result of the operation', () => {
+  it('does the multiply operator update the display with the result of the operation', () => {
     cy.get('#number2').click();
     cy.get('#operator-multiply').click();
     cy.get('#number5').click();
     cy.get('#operator-equals').click()
     cy.get('.display').should('contain', '10')
+
+  })
+
+  it('does the divide operator update the display with the result of the operation', () => {
+    cy.get('#number6').click();
+    cy.get('#operator-divide').click();
+    cy.get('#number2').click();
+    cy.get('#operator-equals').click()
+    cy.get('.display').should('contain', '3')
+
+  })
+
+  it('does the add operator update the display with the result of the operation', () => {
+    cy.get('#number6').click();
+    cy.get('#operator-add').click();
+    cy.get('#number2').click();
+    cy.get('#operator-equals').click()
+    cy.get('.display').should('contain', '8')
+
+  })
+
+  it('does the subtract operator update the display with the result of the operation', () => {
+    cy.get('#number6').click();
+    cy.get('#operator-subtract').click();
+    cy.get('#number2').click();
+    cy.get('#operator-equals').click()
+    cy.get('.display').should('contain', '4')
 
   })
 
@@ -26,6 +53,14 @@ describe("Calculator", () => {
     cy.get('#number2').click();
     cy.get('#operator-equals').click()
     cy.get('.display').should('contain', '8')
+  })
+
+  it('is the output expected for positive numbers', () => {
+    cy.get('#number5').click();
+    cy.get('#operator-subtract').click();
+    cy.get('#number2').click();
+    cy.get('#operator-equals').click()
+    cy.get('.display').should('contain', '3')
   })
 
   it('is the output expected for minus numbers', () => {
@@ -48,19 +83,23 @@ describe("Calculator", () => {
   })
 
   it('is the output expected for large numbers', () => {
-    cy.get('#number3').click();
-    cy.get('#number1').click();
-    cy.get('#number2').click();
-    cy.get('#number3').click();
-    cy.get('#number4').click();
+    cy.get('#number6').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
     cy.get('#operator_add').click();
-    cy.get('#number3').click();
-    cy.get('#number1').click();
-    cy.get('#number2').click();
-    cy.get('#decimal').click();
-    cy.get('#number4').click();
+    cy.get('#number6').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
     cy.get('#operator-equals').click();
-    cy.get('.display').should('contain', '31546.4');
+    cy.get('.display').should('contain', '12000000');
   })
 
   it('what happens when you divide by zero?', () => {
